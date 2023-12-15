4b4e9e598fb5b08cfff283c71b6ac39786c8c0f6
inaki.tazon <inaki.tazon@alumnos.uneatlantico.es>
2023-12-15 18:24:42 +0100

    quitando movidas

--- Clase 1 Cuatri/Progra/Ahoracos/src/com/progra/hangman/base/ShortWord.java
+++
@@ -1,9 +0,0 @@
-package com.progra.hangman.base;
-
-public class ShortWord extends Word{
-	public ShortWord(int id, String word) {
-        super(id,word);
-        this.level = "S";
-        this.maxTries = 3;
-    }
-}
